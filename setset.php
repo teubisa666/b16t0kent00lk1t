45c439ed9dd5dc1bd955cc4c259ca56461522ec8
Yuza <akhmadyuza@gmail.com>
2019-04-19 20:30:44 +0700

    core

---
+++ setset.php
@@ -0,0 +1,9 @@
+<?php
+/**********************
+ * TELAH LULUS SENSOR
+ * ELDERSC0DE FAMILY
+ * 8:29 PM 4/19/2019
+***********************/
+require_once("./api.php");
+new bigToken("XXXXXXXXX"); //GANTI CODE REFF LU
+?>
